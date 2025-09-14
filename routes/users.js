bc3623b74b33bbef7e097265a4b10c18b2aa0c2f
elazar matan <elazarmata8@gmail.com>
2025-09-14 16:17:25 +0300

    Create user authentication routes
    This commit introduces user authentication routes for login and sign-in functionalities. It includes password hashing using bcrypt, JWT token generation for authentication, and basic error handling.

---
+++ routes/users.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import { createUser, getSpecificUser } from '../lib/supabase.js'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const router = express.Router()
+
+router.post('/login',async(req,res) => {
+    try {
+        const user = await getSpecificUser(req.body.username)
+        if(!user.length){
+            return res.status(403).json({ msg: "User not exist" });
+        }
+        const passwordMatch = await bcrypt.compare(req.body.password,user[0].password)
+        if(!passwordMatch){
+            return res.status(403).json({ msg: "the password not match" });
+        }
+        const token = jwt.sign({ username: req.body.username },process.env.SECRETE_KEY,{expiresIn:'7h'})
+        res.json(token)
+    } catch (error) {
+        res.status(500).json({ msg: `server internal error: ${err}` });
+    }
+})
+
+router.post('/signin',async(req,res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password,12)
+        req.body.password = hashedPassword
+        createUser(req.body)
+        const token = jwt.sign({ username: req.body.username },process.env.SECRETE_KEY,{expiresIn:'7h'})
+        console.log(token)
+        res.json(token)
+    } catch (error) {
+        res.status(500).json({ msg: `server internal error: ${err}` });
+    }
+})
+
+router.get('/checkToken',async(req,res) => {
+    res.send('welcome to check token')
+})
+
+export default router
