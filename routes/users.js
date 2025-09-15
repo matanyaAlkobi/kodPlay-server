67006440dc90bdeb16e2fb022e6f235b7a875333
elazar matan <elazarmata8@gmail.com>
2025-09-15 11:05:04 +0300

    ``` feat: Return token in JSON format and implement token verification
    This commit modifies the `/login` and `/signin` routes to return the JWT token within a JSON object. Additionally, it introduces a `/checkToken` route that verifies the provided token and returns the decoded payload.
    ```

--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,7 @@ router.post('/login',async(req,res) => {
             return res.status(403).json({ msg: "the password not match" });
         }
         const token = jwt.sign({ username: req.body.username },process.env.SECRETE_KEY,{expiresIn:'7h'})
-        res.json(token)
+        res.json({token:token})
     } catch (error) {
         res.status(500).json({ msg: `server internal error: ${err}` });
     }
@@ -28,14 +28,22 @@ router.post('/signin',async(req,res) => {
         req.body.password = hashedPassword
         createUser(req.body)
         const token = jwt.sign({ username: req.body.username },process.env.SECRETE_KEY,{expiresIn:'7h'})
-        res.json(token)
+        res.json({token:token})
     } catch (error) {
         res.status(500).json({ msg: `server internal error: ${err}` });
     }
 })
 
 router.get('/checkToken',async(req,res) => {
-    res.send('welcome to check token')
+   try {
+    console.log(req.headers)
+    const token = req.headers['authorization'].split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.SECRETE_KEY);
+    res.json(decoded);
+  } catch (err) {
+    res.status(400).json({ msg: `server internal error: ${err}` });
+  }
 })
 
 export default router
